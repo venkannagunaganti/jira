2077e0b825c5a559c411cb6a7b55d57fcf24e630
vgunaganti <venkanna.gunaganti@icloud.com>
2023-07-18 16:39:39 +0530

    added print_line function

--- JiraTask.py
+++ JiraTask.py
@@ -1,24 +1,41 @@
 import  status
+import inspect
 class LogFile:
+    #writing to the log file
+
     @staticmethod
-    def write_to_log_file(path,msg):
+    def write_to_log_file(path,msg,line_number):
+
+
         with open(path,'a+') as file:
+
+
             file.write(status.file_name+' '+msg+' '+status.time_stamp+' '+'\n')
+    @staticmethod
+    def print_line(path,line_number):
+        with open(path,'r') as file:
+            lines=file.readlines()
+            file.close()
+            line=lines[line_number-1]
+            print(line)
 
 class Error:
+    #funtion to print error message
     @staticmethod
-    def err(path,msg):
-        LogFile.write_to_log_file(path,msg)
+    def err(path,msg,line_number):
+        LogFile.write_to_log_file(path,msg,line_number)
 
 class Warning:
+    #function to print warning message
     @staticmethod
-    def warn(path,msg):
-        LogFile.write_to_log_file(path,msg)
+    def warn(path,msg,line_number):
+        LogFile.write_to_log_file(path,msg,line_number)
 
 class Information:
+    #function to print information
     @staticmethod
-    def info(path,msg):
-        LogFile.write_to_log_file(path,msg)
+    def info(path,msg,line_number):
+        LogFile.write_to_log_file(path,msg,line_number)
 
 error=Error()
 warning=Warning()
@@ -26,6 +43,7 @@ information=Information()
 error_message=status.error_message
 warning_message=status.warning_message
 info=status.info
-error.err(status.path,error_message)
-warning.warn(status.path,warning_message)
-information.info(status.p,info)
+error.err(status.path,error_message,status.line_number)
+warning.warn(status.path,warning_message,status.line_number)
+information.info(status.path,info,status.line_number)
+LogFile.print_line(status.path,status.line_number)
--- status.py
+++ status.py
@@ -4,6 +4,7 @@ msg=''
 path='C:/Users/vgunaganti/PycharmProjects/jira/file.txt'
 with open(path,'w') as file:
     file.write(msg)
+line_number=3
 error_message="this is an error message"
 warning_message="this is warning message"
 info="this is info"

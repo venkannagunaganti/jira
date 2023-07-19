44f08f9b7d6c135d5b595ac1fea59aba34f8fd99
vgunaganti <venkanna.gunaganti@icloud.com>
2023-07-19 17:33:29 +0530

    modified functionalities

--- JiraTask.py
+++ JiraTask.py
@@ -1,24 +1,16 @@
-
 from datetime import datetime
 from pathlib import Path
-
-
 class LogFile:
-    #writing to the log file
-    path = 'C:/Users/vgunaganti/PycharmProjects/jira/file.txt'
-    msg=''
-    with open(path,'w')as file:
-        file.write(msg)
+#writing to the log file function
+  path = 'C:/Users/vgunaganti/PycharmProjects/jira/file.txt'
+  msg = ''
+  with open(path, 'w') as file:
+    file.write(msg)
     @staticmethod
     def write_to_log_file(msg,line_number):
-        # msg = ''
-        # with open(path, 'w') as file:
-        #     file.write(msg)
         p = Path(LogFile.path)
         file_name = p.name
         time_stamp = datetime.fromtimestamp(p.stat().st_mtime).isoformat()
-
-
         with open(LogFile.path,'a+') as file:
             file.write(time_stamp+' '+str(line_number)+' '+file_name+' '+msg+'\n')
     @staticmethod
@@ -29,7 +21,7 @@ class LogFile:
             line=lines[line_number-1]
             print(line+'')
 class Crititcal:
-    # function to write critical msg
+# function to write critical msg
     @staticmethod
     def critical(msg,line_number):
         LogFile.write_to_log_file(msg,line_number)
@@ -38,19 +30,18 @@ class Debug:
     def debug(msg,line_number):
         LogFile.write_to_log_file(msg,line_number)
 class Error:
-    #funtion to print error message
+#funtion to pass error message
     @staticmethod
     def err(msg,line_number):
         LogFile.write_to_log_file(msg,line_number)
-
 class Warning:
-    #function to print warning message
+#function to pass warning message
     @staticmethod
     def warn(msg,line_number):
         LogFile.write_to_log_file(msg,line_number)
 
 class Information:
-    #function to print information
+#function to pass information
     @staticmethod
     def info(msg,line_number):
         LogFile.write_to_log_file(msg,line_number)
--- status.py
+++ status.py
@@ -1,5 +1,7 @@
 import JiraTask
-
+from pathlib import Path
+from datetime import datetime
+#messages
 critical_msg="CRITICAL:the internet is not working"
 debug_msg="DEBUG:This is just a harmless debug message"
 error_message="ERROR:this is an error message"
@@ -7,17 +9,13 @@ warning_message="WARNING:this is warning message"
 info="INFO:this is info"
 file=JiraTask.LogFile.path
 line_number=1
-
-critical=JiraTask.Crititcal()
-debug=JiraTask.Debug()
-error=JiraTask.Error()
-warning=JiraTask.Warning()
-information=JiraTask.Information()
-critical.critical(critical_msg,line_number)
-debug.debug(debug_msg,line_number)
-information.info(info,line_number)
-warning.warn(warning_message,line_number)
-error.err(error_message,line_number)
-
-
+p = Path(JiraTask.LogFile.path)
+file_name = p.name
+time_stamp = datetime.fromtimestamp(p.stat().st_mtime).isoformat()
+#functions calling
+JiraTask.Crititcal().critical(critical_msg,line_number)
+JiraTask.Debug().debug(debug_msg,line_number)
+JiraTask.Information().info(info,line_number)
+JiraTask.Warning().warn(warning_message,line_number)
+JiraTask.Error().err(error_message,line_number)
 # LogFile.print_line(status.path,status.line_number)

b70bd3857f9d3fdb0792945d036106a3dc442a8c
vgunaganti <venkanna.gunaganti@icloud.com>
2023-07-26 20:18:46 +0530

    modified functionalities

---
+++ Message.py
@@ -0,0 +1,25 @@
+
+import os
+from  pathlib import Path
+
+critical="the internet is not working"
+debug="This is just a harmless debug message"
+error="this is an error message"
+warning="this is warning message"
+info="this is info"
+critical_type='CRITICAL'
+debug_type="DEBUG"
+error_type="ERROR"
+warning_type='WARNING'
+info_type='INFO'
+display_type=""
+display="displayed"
+
+# path=os.path.abspath(__file__)
+# print(path)
+# abs_path=Path(path).parent
+# print(abs_path)
+# x="status"
+# y=".log"
+# modified_path=os.path.join(abs_path,x+y)
+# print(modified_path)

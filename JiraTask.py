01033763750054d33449900a7626cdd74342a538
vgunaganti <venkanna.gunaganti@icloud.com>
2023-07-26 22:07:58 +0530

    modified functionalities

--- JiraTask.py
+++ JiraTask.py
@@ -2,37 +2,42 @@ import os.path
 from datetime import datetime
 from pathlib import Path
 import Message
-
-
-
-Log_file:str
+#Declaring global variables
+log_file=""
+p=os.path.abspath(__file__)
+path=Path(p).parent
+path=str(path)+"\\"
+time_stamp=""
 class LogFile:
-#writing to the log file function
+#Creating a new Log file using file name
   @staticmethod
   def create_file(filename):
-      Log_file= filename
-  p=os.path.abspath(__file__)
-  path=Path(p).parent
-  name="status.py".split(".")
-  x=name[0]
-  y=".log"
-  path=path.joinpath(x+y)
-  path=f"{path}"
-  msg = ''
-  with open(path, 'w') as file:
-     file.write(msg)
-  p = Path(path)
-  time_stamp = datetime.fromtimestamp(p.stat().st_mtime).isoformat()
+      global path
+      name=filename.split(".")
+      x=name[0]
+      y=".log"
+      path=str(path)+x+y
+      msg = ''
+      with open(path, 'w') as file:
+         file.write(msg)
 
 
+#writing to the log file
   @staticmethod
   def write_to_log_file(file_name,line_number,msg_type,msg):
+        p = Path(path)
+        # global time_stamp
+        time_stamp = datetime.fromtimestamp(p.stat().st_mtime).isoformat()
 
-        with open(LogFile.path,'a+') as file:
+        with open(path,'a+') as file:
 
-            file.write(f"{LogFile.time_stamp} {file_name}:{line_number} {msg_type}:{msg}\n")
+            file.write(f"{time_stamp} {file_name}:{line_number} {msg_type}:{msg}\n")
+
+#recieving from the user
 @staticmethod
 def msg(file_name,line_no,msg_type,msg):
+    global log_file
+    log_file=file_name
     match msg_type:
         case Message.critical_type:
             LogFile.write_to_log_file(file_name,line_no,Message.critical_type,msg)
@@ -45,7 +50,7 @@ def msg(file_name,line_no,msg_type,msg):
         case Message.error_type:
             LogFile.write_to_log_file(file_name,line_no,Message.error_type,msg)
         case Message.display_type:
-            print(f"{LogFile.time_stamp} {file_name}:{line_no} {Message.display}")
+            print(f"{time_stamp} {file_name}:{line_no} {Message.display}")
 
         case unknown_command:
             print(f"invalid message:{unknown_command} at lineno:{line_no}")
--- Message.py
+++ Message.py
@@ -1,7 +1,3 @@
-
-import os
-from  pathlib import Path
-
 critical="the internet is not working"
 debug="This is just a harmless debug message"
 error="this is an error message"
@@ -14,12 +10,3 @@ warning_type='WARNING'
 info_type='INFO'
 display_type=""
 display="displayed"
-
-# path=os.path.abspath(__file__)
-# print(path)
-# abs_path=Path(path).parent
-# print(abs_path)
-# x="status"
-# y=".log"
-# modified_path=os.path.join(abs_path,x+y)
-# print(modified_path)

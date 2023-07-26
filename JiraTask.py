ae8b96c6f652aca9f9d8081ef12589da29d877ae
vgunaganti <venkanna.gunaganti@icloud.com>
2023-07-26 20:17:36 +0530

    modified functionalities

--- JiraTask.py
+++ JiraTask.py
@@ -1,37 +1,51 @@
+import os.path
 from datetime import datetime
 from pathlib import Path
 import Message
+
+
+
+Log_file:str
 class LogFile:
 #writing to the log file function
-  path = 'C:/Users/vgunaganti/PycharmProjects/jira/file.txt'
-  p = Path(path)
-  time_stamp = datetime.fromtimestamp(p.stat().st_mtime).isoformat()
+  @staticmethod
+  def create_file(filename):
+      Log_file= filename
+  p=os.path.abspath(__file__)
+  path=Path(p).parent
+  name="status.py".split(".")
+  x=name[0]
+  y=".log"
+  path=path.joinpath(x+y)
+  path=f"{path}"
   msg = ''
   with open(path, 'w') as file:
-    file.write(msg)
-    @staticmethod
-    def write_to_log_file(file_name,msg,line_number):
+     file.write(msg)
+  p = Path(path)
+  time_stamp = datetime.fromtimestamp(p.stat().st_mtime).isoformat()
+
+
+  @staticmethod
+  def write_to_log_file(file_name,line_number,msg_type,msg):
 
         with open(LogFile.path,'a+') as file:
 
-            file.write(f"{LogFile.time_stamp} {file_name}:{line_number} {msg}\n")
+            file.write(f"{LogFile.time_stamp} {file_name}:{line_number} {msg_type}:{msg}\n")
 @staticmethod
-def msg(file_name,msg,line_no):
-
-   for i in range(1):
-    match msg:
-        case Message.critical_msg:
-            LogFile.write_to_log_file(file_name,Message.critical_msg,line_no)
-        case Message.debug_msg:
-            LogFile.write_to_log_file(file_name,Message.debug_msg,line_no)
-        case Message.warning_message:
-            LogFile.write_to_log_file(file_name,Message.warning_message,line_no)
-        case Message.info:
-            LogFile.write_to_log_file(file_name,Message.info,line_no)
-        case Message.error_message:
-            LogFile.write_to_log_file(file_name,Message.error_message,line_no)
-        case Message.Display:
-            print(f"{LogFile.time_stamp} {file_name}:{line_no} {Message.Display}")
+def msg(file_name,line_no,msg_type,msg):
+    match msg_type:
+        case Message.critical_type:
+            LogFile.write_to_log_file(file_name,line_no,Message.critical_type,msg)
+        case Message.debug_type:
+            LogFile.write_to_log_file(file_name,line_no,Message.debug_type,msg)
+        case Message.warning_type:
+            LogFile.write_to_log_file(file_name,line_no,Message.warning_type,msg)
+        case Message.info_type:
+            LogFile.write_to_log_file(file_name,line_no,Message.info_type,msg)
+        case Message.error_type:
+            LogFile.write_to_log_file(file_name,line_no,Message.error_type,msg)
+        case Message.display_type:
+            print(f"{LogFile.time_stamp} {file_name}:{line_no} {Message.display}")
 
         case unknown_command:
             print(f"invalid message:{unknown_command} at lineno:{line_no}")
--- status.py
+++ status.py
@@ -3,8 +3,9 @@ import Message
 import inspect
 import os
 #messages
-path="C:/Users/vgunaganti/PycharmProjects/jira/status.py"
+path=os.path.abspath(__file__)
 file_name=os.path.basename(path)
+
 @staticmethod
 def line_num():
     file=inspect.currentframe()
@@ -12,13 +13,13 @@ def line_num():
     return i.lineno
 
 #functions calling
-JiraTask.msg(file_name,Message.critical_msg,line_num())
-JiraTask.msg(file_name,Message.debug_msg,line_num())
-JiraTask.msg(file_name,"Message.warning_message",line_num())
-JiraTask.msg(file_name,Message.info,line_num())
-JiraTask.msg(file_name,Message.error_message,line_num())
-JiraTask.msg(file_name,Message.Display,line_num())
-
+JiraTask.LogFile.create_file(file_name)
+JiraTask.msg(file_name,line_num(),Message.critical_type,Message.critical)
+JiraTask.msg(file_name,line_num(),Message.debug_type,Message.debug)
+JiraTask.msg(file_name,line_num(),Message.warning_type,Message.warning)
+JiraTask.msg(file_name,line_num(),Message.info_type,Message.info)
+JiraTask.msg(file_name,line_num(),Message.error_type,Message.error)
+JiraTask.msg(file_name,line_num(),Message.display_type,Message.display)
 
 
 
